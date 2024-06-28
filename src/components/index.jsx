67770853faf2a69fe66cd61a777fb10d801433c8
kaustubhgandhi10 <kaustubh.gandhi10@gmail.com>
2024-06-28 18:18:29 +0530

    upload project

---
+++ src/components/index.jsx
@@ -0,0 +1,18 @@
+import UseLocalStorage from "./useLocalStorage";
+import "./theme.css";
+
+export default function LightDarkMode() {
+  const [theme, setTheme] = UseLocalStorage("theme", "dark");
+
+  function handleToggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+  return (
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <p>Hello World...!</p>
+        <button onClick={handleToggleTheme}>Change Theme</button>
+      </div>
+    </div>
+  );
+}
